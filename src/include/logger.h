c5ab949bfaf1d70b0dcd0fdf4ef239d964890b4e
Anush Krishnan <anush@bu.edu>
2012-08-31 14:55:14 -0400

    Corrected all the copyright statements.

--- src/include/logger.h
+++ src/include/logger.h
@@ -1,5 +1,5 @@
 /*
-*  Copyright (C) 2011 by Anush Krishnan, Simon Layton, Lorena Barba
+*  Copyright (C) 2012 by Anush Krishnan, Simon Layton, Lorena Barba
 *
 *  Permission is hereby granted, free of charge, to any person obtaining a copy
 *  of this software and associated documentation files (the "Software"), to deal
