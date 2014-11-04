52ee615533bab4b338fea44f7549fadcebdef8ce
mesnardo <mesnardo@gwu.edu>
2014-11-04 15:16:29 -0500

    Update Doxygen documentation with conventions

--- src/parameterDB.h
+++ src/parameterDB.h
@@ -1,8 +1,9 @@
 /***************************************************************************//**
-* \file parameterDB.h
-* \author Krishnan, A. (anush@bu.edu)
-* \brief Database that contains all the simulation parameters
-*/
+ * \file parameterDB.h
+ * \author Anush Krishnan (anush@bu.edu)
+ * \brief Declaration of the class \c property.
+ */
+
 
 #pragma once
 
@@ -14,10 +15,11 @@
 #include <iostream>
 #include "boundaryCondition.h"
 
+
 /**
-* \class property
-* \brief Generic property storage
-*/
+ * \class property
+ * \brief Contains information about a property stored in a generic way.
+ */
 class property
 {
 	template <typename T> T getInternal();
@@ -30,42 +32,42 @@ class property
 
 public:
 	/**
-	* \brief Constructor of the class \c property.
-	*
-	* Initialize the memory to zero.
-	*
-	*/
+	 * \brief Constructor of the class \c property.
+	 *
+	 * It initializes the memory to zero.
+	 *
+	 */
 	property()
 	{
 		memset(value, 0, 256);
 	}
 	
 	/**
-	* \brief Get the value of the property as a given type
-	*/
+	 * \brief Gets the value of the property as a given type.
+	 */
 	template <typename T> T get();
 	
 	/**
-	* \brief Set the value of the property given a type
-	*/
+	 * \brief Sets the value of the property given a type.
+	 */
 	template <typename T> void set(T v);
 
 	/**
-	* \brief Return a string describing the value of property as appropriate
-	*/
+	 * \brief Returns a string describing the value of property as appropriate.
+	 */
 	const char *print();
 };
 
 /**
-* \typedef componentParameter
-* \brief Map from a string to a \c property
-*/
+ * \typedef componentParameter
+ * \brief Map from a string to a \c property.
+ */
 typedef std::map<std::string, property> componentParameter;
 
 /**
-* \typedef parameterDB
-* \brief Map from a string to a \c componentParameter
-*/
+ * \typedef parameterDB
+ * \brief Map from a string to a \c componentParameter.
+ */
 typedef std::map<std::string, componentParameter> parameterDB;
 
 // output a DB
