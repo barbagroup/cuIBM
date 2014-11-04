c723875e377f92710bae4e55fbafb7ba8ea6220c
mesnardo <mesnardo@gwu.edu>
2014-11-04 14:23:56 -0500

    Update Doxygen documentation with conventions

--- src/boundaryCondition.h
+++ src/boundaryCondition.h
@@ -1,8 +1,9 @@
 /***************************************************************************//**
-* \file boundaryCondition.h
-* \author Krishnan, A. (anush@bu.edu)
-* \brief Definition of the class \c boundaryCondition
-*/
+ * \file boundaryCondition.h
+ * \author Anush Krishnan (anush@bu.edu)
+ * \brief Definition of the class \c boundaryCondition.
+ */
+
 
 #pragma once
 
@@ -11,10 +12,11 @@
 #include "types.h"
 #include "parameterDB.h"
 
+
 /**
-* \class boundaryCondition
-* \brief Store the boundary conditions for a given system
-*/
+ * \class boundaryCondition
+ * \brief Stores the boundary conditions for a given system.
+ */
 class boundaryCondition
 {
 public:
@@ -22,21 +24,20 @@ public:
 	real  value; ///< numerical value associated with the boundary condition
 	
 	/**
-	* \brief Constructor of the class \c boundaryCondition.
-	*
-	* Initialize with a Dirichlet-type boundary condition
-	* with a value sets to zero.
-	*
-	*/
+	 * \brief Constructor of the class \c boundaryCondition.
+	 *
+	 * Boundary condition initialized with a Dirichlet-type with
+	 * with a value sets to zero.
+	 *
+	 */
 	boundaryCondition() : type(DIRICHLET), value(0) {};
 
 	/**
-	* \brief Other constructor of the class \c boundaryCondition.
-	*
-	* Initialize with a given boundary condition type
-	* and a given value.
-	*
-	*/
+	 * \brief Other constructor of the class \c boundaryCondition.
+	 *
+	 * Boundary condition initialized with a given type and a given value.
+	 *
+	 */
 	boundaryCondition(bcType _type, real _value) : type(_type), value(_value) {}; 
 
   /*const char *print()
