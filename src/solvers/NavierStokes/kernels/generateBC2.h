a923d18a2f559eb608140bfa3fcbc68da714a668
mesnardo <mesnardo@gwu.edu>
2014-11-06 19:25:13 -0500

    Review Doygen block comments

--- src/solvers/NavierStokes/kernels/generateBC2.h
+++ src/solvers/NavierStokes/kernels/generateBC2.h
@@ -1,40 +1,35 @@
 /***************************************************************************//**
-* \file generateBC2.h
-* \author Krishnan, A. (anush@bu.edu)
-* \brief Declaration of the kernels required to generate elements of vector \c bc2
-*/
+ * \file generateBC2.h
+ * \author Anush Krishnan (anush@bu.edu)
+ * \brief Declaration of the kernels to generate elements of the right hand-side
+ *        of the Poisson solver.
+ */
+
 
 #pragma once
 
 #include <types.h>
 
+
 /**
-* \namespace kernels
-* \brief Contain all custom-written CUDA kernels
-*/
+ * \namespace kernels
+ * \brief Contains all custom-written CUDA kernels.
+ */
 namespace kernels
 {
 
-/**
-* \brief Kernel to generate Dirichlet boundary condition term
-*		coming from the discrete divergence operator
-*		at bottom and top v-velocity boundary points
-*/
+// compute inhomogeneous terms of the discrete divergence operator
+// from the bottom and top boundaries at the v-velocity locations
 __global__
 void fillBC2_v(real *bc2, real *yminus, real *yplus, real *dx, int nx, int ny);
 
-/**
-* \brief Kernel to generate Dirichlet boundary condition term
-*		coming from the discrete divergence operator
-*		at left and right u-velocity boundary points
-*/
+// compute inhomogeneous terms of the discrete divergence operator
+// from the left and right boundaries at the u-velocity locations
 __global__
 void fillBC2_u(real *bc2, real *xminus, real *xplus, real *dy, int nx, int ny);
 
-/**
-* \brief Kernel to generate no-slip condition terms (x- and y- components)
-*		at a body-point location
-*/
+// compute inhomogeneous terms of the discrete divergence operator
+// from the no-slip constraint at the body-point locations
 __global__
 void fillBC2_uvB(real *bc2, real *uB, real *vB, int totalPoints, int nx, int ny);
 
